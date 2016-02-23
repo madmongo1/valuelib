d9dcbec18d1b932022b943cd720f484967b8106d
Richard Hodges <richard@trustportal.org>
2016-02-23 10:45:25 +0100

    enhancement - added any_printer

---
+++ debug/include/valuelib/debug/any_printer.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <boost/any.hpp>
+#include <iostream>
+#include <memory>
+#include <typeindex>
+
+namespace value { namespace debug {
+
+    struct any_printer
+    {
+        template<class T>
+        using print_function = std::function<void (std::ostream& os, const T& val)>;
+        
+        struct concept
+        {
+            virtual void print(std::ostream&, const boost::any& a) const = 0;
+            virtual ~concept() = default;
+        };
+        
+        template<class T> struct model final : concept {
+            model(print_function<T>&& f) : _f(std::move(f)) {}
+            void print(std::ostream&os, const boost::any& a) const override
+            {
+                _f(os, boost::any_cast<const T&>(a));
+            }
+        private:
+            print_function<T> _f;
+        };
+        
+        struct impl;
+        static impl& get_impl();
+        
+        static std::ostream& print(std::ostream& os, const boost::any& a);
+        static bool add_printer_impl(std::type_index ti, std::unique_ptr<concept>&& pc);
+        
+        template<class T>
+        static bool add_printer(print_function<T> func)
+        {
+            return add_printer_impl(typeid(T), std::make_unique<model<T>>(std::move(func)));
+        }
+        
+    };
+
+}}
+
+namespace boost {
+    inline std::ostream& operator<<(std::ostream& os, const boost::any& a)
+    {
+        return value::debug::any_printer::print(os, a);
+    }
+}
---
+++ debug/src/any_printer.cpp
@@ -0,0 +1,52 @@
+#include <valuelib/debug/any_printer.hpp>
+#include <unordered_map>
+#include <valuelib/debug/demangle.hpp>
+
+namespace value { namespace debug {
+  
+    struct any_printer::impl {
+        using map_type = std::unordered_map<std::type_index, std::unique_ptr<concept>>;
+        mutable std::mutex _mutex;
+        map_type _map;
+        
+        bool add_printer(std::type_index ti, std::unique_ptr<concept> printer)
+        {
+            auto lock = std::unique_lock<std::mutex>(_mutex);
+            return _map.emplace(ti, std::move(printer)).second;
+        }
+        
+        void print(std::ostream& os, std::type_index ti, const boost::any& a) const
+        {
+            try {
+            auto lock = std::unique_lock<std::mutex>(_mutex);
+            const auto& printer = _map.at(ti);
+            lock.unlock();
+            printer->print(os, a);
+            }
+            catch(const std::out_of_range&)
+            {
+                os << "unregistered object of type: " << value::debug::demangle(ti);
+            }
+        }
+    };
+    
+    auto any_printer::get_impl() -> impl&
+    {
+        static impl _{};
+        return _;
+    }
+
+    bool any_printer::add_printer_impl(std::type_index ti, std::unique_ptr<concept>&& pc)
+    {
+        return get_impl().add_printer(ti, std::move(pc));
+    }
+
+    std::ostream& any_printer::print(std::ostream& os, const boost::any& a)
+    {
+        auto& impl = get_impl();
+        impl.print(os, a.type(), a);
+        return os;
+    }
+
+
+}}
