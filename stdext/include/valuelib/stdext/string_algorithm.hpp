c053cd2d6f8d1293362f12be367f3c00a1bb51fc
Richard Hodges <richard@trustportal.org>
2016-03-07 17:37:06 +0100

    enhancement - added join(sep, iter, iter) algorithm

--- stdext/include/valuelib/stdext/string_algorithm.hpp
+++ stdext/include/valuelib/stdext/string_algorithm.hpp
@@ -18,7 +18,45 @@ namespace value {
         find(String& s, typename ValueType<String>::value_type ch, typename String::size_type pos = 0)
         {
             return s.find(ch, pos);
-        } 
+        }
+        
+        template<class IterValueType>
+        struct join_target
+        {
+            using type = IterValueType;
+        };
+        
+        template<>
+        struct join_target<const char*>
+        {
+            using type = std::string;
+        };
+        
+        template<size_t N>
+        struct join_target<const char [N]>
+        {
+            using type = std::string;
+        };
+        
+        template<class IterValueType>
+        using JoinTargetType = typename join_target<IterValueType>::type;
+        
+        
+        
+        template<class Iter, class Separator>
+        auto join(const Separator& sep, Iter first, Iter last)
+        {
+            auto result = JoinTargetType<typename Iter::value_type>();
+            if (first != last)
+            {
+                result = *first++;
+                while (first != last) {
+                    result += sep;
+                    result += *first++;
+                }
+            }
+            return result;
+        }
         
 	}
 }
